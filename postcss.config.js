9f707d5a1746ff2125982ba41791e3d28684a76c
NirPatrick <105299848+NirPatrick@users.noreply.github.com>
2025-02-06 19:36:55 +0300

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js (en mode ES module)
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App p-8 bg-slate-500 text-xl text-center">
+      <h1>Bonjour, Tailwind CSS avec Vite et React !</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
